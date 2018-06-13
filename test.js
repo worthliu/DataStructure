b78afacfb32600c38d5cc902da8ac9c4bda4432d
liuzcmf <liuzcmf@fansfinancial.com>
2018-06-13 09:02:53 +0800

    graph data structure

--- test.js
+++ test.js
@@ -84,9 +84,32 @@
 // console.log(unionSet.values());
 
 
-var ht = require("HashTable");
+// var ht = require("HashTable");
 
-var hashTable = new ht.HashTable();
-hashTable.put("Gandalf", "gandalf@gmail.com");
-hashTable.put("John", "john@gmail.com");
-hashTable.put("Tyrion", "tyrion@gmail.com");
+// var hashTable = new ht.HashTable();
+// hashTable.put("Gandalf", "gandalf@gmail.com");
+// hashTable.put("John", "john@gmail.com");
+// hashTable.put("Tyrion", "tyrion@gmail.com");
+
+
+var gp = require("Graph");
+
+var graph = new gp.Graph();
+var myVertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+
+for(var i = 0; i < myVertices.length; i++){
+	graph.addVertex(myVertices[i]);
+}
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("C", "D");
+graph.addEdge("C", "G");
+graph.addEdge("D", "G");
+graph.addEdge("D", "H");
+graph.addEdge("B", "E");
+graph.addEdge("B", "F");
+graph.addEdge("E", "I");
+
+console.log(graph.printGraph());
